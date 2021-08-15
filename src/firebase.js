f33d7ceae9d6a8a93fb5ddc74e0ebbde4636f868
Lee Sang Jin <lee3jjang@gmail.com>
2021-08-15 14:13:40 +0900

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/app';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyB-FUcq-OxmckTxewkPTpWGo_QfFlElOHA",
-    authDomain: "nwitter-5dd26.firebaseapp.com",
-    projectId: "nwitter-5dd26",
-    storageBucket: "nwitter-5dd26.appspot.com",
-    messagingSenderId: "366615868371",
-    appId: "1:366615868371:web:970ae7b3160f1a9b817526",
-    measurementId: "G-KPJ2J2W8RD"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase

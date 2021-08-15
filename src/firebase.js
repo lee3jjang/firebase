aceeca10316cfddf4baf2e2459db23601709ca63
Lee Sang Jin <lee3jjang@gmail.com>
2021-08-15 13:47:14 +0900

    install firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB-FUcq-OxmckTxewkPTpWGo_QfFlElOHA",
+    authDomain: "nwitter-5dd26.firebaseapp.com",
+    projectId: "nwitter-5dd26",
+    storageBucket: "nwitter-5dd26.appspot.com",
+    messagingSenderId: "366615868371",
+    appId: "1:366615868371:web:970ae7b3160f1a9b817526",
+    measurementId: "G-KPJ2J2W8RD"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
